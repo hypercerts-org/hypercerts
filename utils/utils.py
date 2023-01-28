1992031e8cd04ffee4b35b3e1a5374190bda08fe
ccerv1 <carl.cervone@gmail.com>
2023-01-28 10:30:22 -0500

    fix: cleanup module names and readme

--- utils/utils.py
+++ utils/utils.py
@@ -3,3 +3,9 @@ from datetime import datetime
 
 def datify(u):
     return datetime.fromtimestamp(u).strftime('%Y-%m-%d') if u else "Indefinite"
+
+def shorten_address(a):
+    if isinstance(str, a):
+        return a[:7]
+    else:
+        return "unknown"
