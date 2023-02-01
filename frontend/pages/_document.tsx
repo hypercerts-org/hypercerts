9055bdd47b42f3caef4f46fe22cfc1475b8cdcfa
Raymond Cheng <me@raymondcheng.net>
2023-01-31 18:38:01 -0800

    feat: add favicon (#135)

---
+++ frontend/pages/_document.tsx
@@ -0,0 +1,27 @@
+import Document, { DocumentContext, DocumentInitialProps, Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }  
+}
+
+export default MyDocument
