39695e6bc5c20181b1d24d033e29afffd476975b
ccerv1 <carl.cervone@gmail.com>
2023-02-06 22:42:39 -0500

    fix: add proper  configs and env variables

--- utils/get_grants_data.py
+++ utils/get_grants_data.py
@@ -1,25 +1,28 @@
-from datetime import datetime
+from dotenv import load_dotenv
 import json
-import requests
+import os
 import pandas as pd
-import sys
+import requests
 
+
+load_dotenv()
+API_KEY = os.environ['THEGRAPH_API_KEY']
+
+with open("config.json") as config_file:
+    CONFIG = json.load(config_file)    
+SETTINGS = CONFIG["gitcoin_settings"]
+ROUNDS = SETTINGS["matching_pool_settings"]
 
 # Directory for keeping csv files returned by the module
-OUT_DIR = 'csv/'
+OUT_DIR = SETTINGS["path_to_round_csv_dir"]
 
-# Gitcoin Round Manager subgraph ID on The Graph
-SUBGRAPH = "BQXTJRLZi7NWGq5AXzQQxvYNa5i1HmqALEJwy3gGJHCr"
-ROUNDS = {
-    'climate': '0x1b165fe4da6bc58ab8370ddc763d367d29f50ef0', 
-    'oss': '0xd95a1969c41112cee9a2c931e849bcef36a16f4c', 
-    'eth_infra': '0xe575282b376e3c9886779a841a2510f1dd8c2ce4'
-}
 
 # Get the Round Data by Querying TheGraph 
-def get_round_data(round_id, api_key):
-    # URL with the endpoint of the round manager subgraph for mainnet 
-    url = f"https://gateway.thegraph.com/api/{api_key}/subgraphs/id/{SUBGRAPH}"
+def get_round_data(round_id):
+
+    # URL with the endpoint of the Gitcoin round manager subgraph for mainnet 
+    subgraph = SETTINGS["subgraph_id"]
+    url = f"https://gateway.thegraph.com/api/{API_KEY}/subgraphs/id/{subgraph}"
 
     # Construct the GraphQL query
     query = '''
@@ -67,7 +70,7 @@ def get_round_data(round_id, api_key):
 
 def retrieve_ipfs_file(cid):
     # Build the URL to the file on the Cloudflare IPFS gateway
-    url = f"https://cloudflare-ipfs.com/ipfs/{cid}"
+    url = f"{CONFIG['hosted_cid_base_url']}{cid}"
     
     try:    
         # Send a GET request to the URL
@@ -83,40 +86,10 @@ def retrieve_ipfs_file(cid):
         return None
 
 
-def dataframe_to_sql(df, file_path):
-    # Create the template for the SQL query
-    template = """WITH grantees AS (
-    SELECT title, address, status FROM (VALUES {}) x (title, address, status)
-    )
-    SELECT * FROM grantees;
-    """
-    # Create a list to store the values
-    values_list = []
-    # Iterate through the DataFrame
-    for _, row in df.iterrows():
-        # Check for missing values in the specified columns
-        if not row[['title', 'recipient', 'status']].isnull().any():
-            # If there are no missing values, add the row to the values list
-            # replace single quotes with double quotes in title column
-            title = row["title"].replace("'", "''")
-            # Replace the first character of recipient with '\' and save it under the column named address
-            address = '\\' + row["recipient"][1:]
-            values_list.append("('{}', '{}' ::bytea, '{}')".format(title, address, row["status"]))
-
-    # Create the values string
-    values = ", ".join(values_list)
-    # Write the query to the file
-    with open(file_path, "w") as f:
-        f.write(template.format(values))
-
-
-def main(round_name, api_key):
-    # Get the current time
-    current_time = datetime.now().strftime("%Y_%m_%d-%H_%M_%S")
+def main(round_id):
 
     # Pull the Data from TheGraph and save it to a dataframe
-    round_id = ROUNDS.get(round_name)
-    df = get_round_data(round_id, api_key)
+    df = get_round_data(round_id)
 
     # Add IPFS data with the grantees name and address 
     df['ipfs_data'] = df['pointer'].apply(retrieve_ipfs_file)
@@ -124,20 +97,12 @@ def main(round_name, api_key):
     df['title'] = df['ipfs_data'].apply(lambda x: x["application"]["project"]["title"])
    
     # Save the data as a CSV file
-    csv_file_name = '{}{}_{}_data.csv'.format(OUT_DIR, current_time, round_name)
+    round_name = ROUNDS[round_id]["name"].replace(" ","")
+    csv_file_name = '{}{}_ProjectData.csv'.format(OUT_DIR, round_name)
     df.to_csv(csv_file_name, index=False)
 
 
 if __name__ == "__main__":
 
-    if len(sys.argv) == 3:
-        round_name = sys.argv[2].lower()
-        if not ROUNDS.get(round_name):
-            print("Please enter a valid round name.")
-            print("Options are:", list(ROUNDS.keys()))
-        else:
-            api_key  = sys.argv[1]
-            main(round_name, api_key)
-            print("\n\nDone!")        
-    else:
-        print("Enter API key followed by a valid round name")
+    for round_id in ROUNDS.keys():
+        main(round_id)       
