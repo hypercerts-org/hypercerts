4dbf0a5772cb993ec1f9aa15c87d804d09fc8e9a
Raymond Cheng <me@raymondcheng.net>
2023-02-08 17:58:45 -0800

    fix: reload dashboard after claim all fractions

--- frontend/components/claim-all-fractions-button.tsx
+++ frontend/components/claim-all-fractions-button.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@mui/material";
+import { useRouter } from "next/router";
+import { useAccount } from "wagmi";
 import {
   useGetAllEligibility,
   useMintFractionAllowlistBatch,
 } from "../hooks/mintFractionAllowlistBatch";
-import { useAccount } from "wagmi";
 
 export const ClaimAllFractionsButton = ({
   text = "Claim all fractions",
@@ -13,10 +14,12 @@ export const ClaimAllFractionsButton = ({
   className?: string;
 }) => {
   const { address } = useAccount();
+  const router = useRouter();
   const { data: claimIds } = useGetAllEligibility(address || "");
   const { write } = useMintFractionAllowlistBatch({
     onComplete: () => {
       console.log("Minted all of them");
+      router.reload();
     },
   });
   return (
