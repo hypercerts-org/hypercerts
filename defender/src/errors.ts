66b7656af9a6538a2a5cfa4ccb25c5006ba1102f
Raymond Cheng <me@raymondcheng.net>
2023-02-23 12:23:09 -0800

    fix: Defender scripts
    * Hardcode the HypercertMinterABI into defender due to autotask
      restricted environment
    * Remove all dependencies on @hypercerts-org/* packages
    * Better logging and data validation for debugging
    * Use Alchemy provider for retrieving transaction data
    * Get AllowlistCreated events from Alchemy. Not sure what happened but
      event.request.body.matchReasons are no longer being populated from
      OpenZeppelin
    * More info on devops playbook

--- defender/src/errors.ts
+++ defender/src/errors.ts
@@ -7,3 +7,13 @@ export class ApiError extends Error {}
  * Misconfigured. Check your environment variables or `src/config.ts`
  */
 export class ConfigError extends Error {}
+
+/**
+ * This is a pathway that hasn't been implemented yet
+ */
+export class NotImplementedError extends Error {}
+
+/**
+ * This is a pathway that hasn't been implemented yet
+ */
+export class MissingDataError extends Error {}
