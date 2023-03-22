97bd1b65de29623acf34039a489b291a8182b23d
ccerv1 <carl.cervone@gmail.com>
2023-03-22 09:53:47 -0400

    feat: grab optimism wallet stats from etherscan

--- utils/optimism/serialize_address_data.py
+++ utils/optimism/serialize_address_data.py
@@ -7,13 +7,17 @@ import requests
 load_dotenv()
 OPTIMISM_KEY  = os.environ['OPTIMISM_ETHERSCAN_API_KEY']
 ETHERSCAN_KEY = os.environ['ETHERSCAN_KEY']
+HEADERS = {
+    "Accept": "application/json",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36"
+}
 
 PROJECT_DATA  = "data/projects/"
 JSONDATA_PATH = "data/optimism_project_data.json"
 
 
 def load_projects():
-    paths = os.listdir(PROJECT_DATA)
+    paths = [p for p in os.listdir(PROJECT_DATA) if p[-5:] == '.json']
     data = []
     for path in paths:
         project = json.load(open(PROJECT_DATA+path))
@@ -31,9 +35,11 @@ def optimism_scan(addr):
                         f"&address={addr}",
                         "&tag=latest",
                         f"&apikey={OPTIMISM_KEY}"])    
-    
-    response = requests.get(query, headers={"Accept": "application/json"})
+
+    response = requests.get(query, headers=HEADERS)
     data = response.json()
+
+    
     has_address = data['message'] == 'OK'
     balance = float(data['result'][0]['balance']) / 1000000000000000000. if has_address else 0
     
@@ -48,7 +54,11 @@ def run_address_stats():
     data = load_projects()
     for project in data:
         address = project.get("address")
-        project.update(optimism_scan(address))
+        try:
+            project.update(optimism_scan(address))
+        except:
+            print("Error:", address)
+            
 
     out_file = open(JSONDATA_PATH, "w")
     json.dump(data, out_file, indent=4)                
