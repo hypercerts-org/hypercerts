f323c555762443a92c833604f1a2a598356b6c94
Raymond Cheng <me@raymondcheng.net>
2023-04-05 17:53:30 -0700

    feat: Add Sentry integration (#516)
    * Setup the frontend with Sentry
    * Note: This does not current discriminate between dev, staging,
      testnet, production, mainnet etc.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,11 @@
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { withSentryConfig } = require("@sentry/nextjs");
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
@@ -10,3 +18,8 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+module.exports = withSentryConfig(
+  module.exports,
+  { silent: true },
+  { hideSourcemaps: true },
+);
