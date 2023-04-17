db01d9445c5a1bbe3d5732c27db616ba1af9eda9
ccerv1 <carl.cervone@gmail.com>
2023-04-17 10:57:13 -0400

    fix: better data pipes between TheGraph and Supabase

--- utils/supabase/claims_metadata_mapper.py
+++ utils/supabase/claims_metadata_mapper.py
@@ -1,134 +1,152 @@
-from datetime import datetime
-from dotenv import load_dotenv
-import json
 import os
+import json
 import pandas as pd
 import requests
+from datetime import datetime
+from dotenv import load_dotenv
 from supabase import create_client, Client
-import sys
 
 load_dotenv()
-url = os.environ.get("SUPABASE_URL")
-key = os.environ.get("SUPABASE_KEY")
-supabase: Client = create_client(url, key)
+SUPABASE_URL = os.environ.get("SUPABASE_URL")
+SUPABASE_KEY = os.environ.get("SUPABASE_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-table_name = "claims-metadata-mapping"
-outpath = "data/claimsData.json"
-
-def datify(u):
-   fmt = "%Y-%m-%d %H:%M:%S"
-   return datetime.fromtimestamp(int(u)).strftime(fmt)
-
-# graph the hypercert metadata from IPFS
-def retrieve_ipfs_file(cid):
-
-   # use the cloudlare gateway
-   url = "https://cloudflare-ipfs.com/ipfs/" + cid
-   print("Fetching:", url)
-
-   try:    
-     response = requests.get(url)
-     response.raise_for_status()
-     data = json.loads(response.content)
-     return data
-
-   except requests.exceptions.HTTPError as e:
-     print(e)
-
-   return None
+TABLE_NAME = "claims-metadata-mapping"
+CSV_FILEPATH = "data/claimsData.csv"
 
 
-# Get the latest claim data by querying TheGraph 
-def get_claims_data():
-
-   # Construct the GraphQL query
-   query = '''
-     {
-        claims(
-          first: 300, 
-          where: {totalUnits_not: "10000"}
-        ) {
-          id
-          creation
-          totalUnits
-          creator
-          uri
-        }
-      }
-   '''
-   # Query TheGraph API for the round's data by POST request
-   url = "https://api.thegraph.com/subgraphs/name/hypercerts-admin/hypercerts-optimism-mainnet"
-   response = requests.post(url, json={'query': query})
-   data = response.json()
-
-   # Iterate through the JSON object and add data to our list
-   claims = []
-   for claim in data['data']['claims']:
-      cid = claim['uri'].replace("ipfs://", "")
-      claims.append({
-         'claimId': claim['id'],
-         'createdAt': claim['creation'],
-         'totalUnits': claim['totalUnits'],
-         'creatorAddress': claim['creator'],
-         'metadataCid': cid,
-         'metadata': retrieve_ipfs_file(cid)
-      })
-   print(f"Total of {len(claims)} claims extracted.")
-
-   out_file = open(outpath, "w")
-   json.dump(claims, out_file, indent=4)                
-   out_file.close()   
-
-   return claims
+def timestamp_to_date_string(timestamp: str) -> str:
+    """Converts a Unix timestamp to a formatted date string."""
+    fmt = "%Y-%m-%d %H:%M:%S"
+    return datetime.fromtimestamp(int(timestamp)).strftime(fmt)
 
 
-# populate the supabase with existing claims
-def make_initial_csv():
-
-   claims_data = json.load(open(outpath))
-   records = []
-   for claim in claims_data:
-      records.append({
-         "createdAt": int(claim["createdAt"]),
-         "claimId": claim["claimId"],
-         "title": claim["metadata"]["name"],
-         "creatorAddress": claim["creatorAddress"],
-         "totalUnits": int(claim["totalUnits"]),
-         "date": datify(claim["createdAt"])
-      })    
-   df = pd.DataFrame(records)
-   df.sort_values(by="createdAt", inplace=True)
-   df.reset_index(inplace=True, drop=True)
-   df.index.name = "id"
-   df.to_csv(outpath.replace(".json", ".csv"))
-
-
-# fetch all claims from the database
-def fetch_supabase_claims():
-
-   data, _ = (supabase 
-               .table(table_name)
-               .select('claimId, title')
-               .execute())
-   claims = data[1]
-   count = len(claims)
-   print(f"Fetched {count} claims from the database.")
-   return claims
+def get_claims_with_offset(offset: int = 0) -> list:
+    """Fetches claims from The Graph with a given offset."""
+    query = f'''
+        {{
+            claims(          
+                first: 1000
+                skip: {offset}
+                orderBy: creation
+            ) {{
+                id
+                totalUnits
+                uri
+                creator
+                creation    
+            }}
+        }}
+    '''
+    url = "https://api.thegraph.com/subgraphs/name/hypercerts-admin/hypercerts-optimism-mainnet"
+    response = requests.post(url, json={'query': query})
+    json_data = response.json()
+    data = json_data['data']['claims']
+    return data
 
 
-# TODO: write a script that looks for new claims and updates the database
-#def update_supabase():
-#       data, count = (supabase
-#                      .table(table_name)
-#                      .insert({"address": addr, "claimId": claim_id})
-#                      .execute())
-#    print(f"Successfully added {len(addresses)} rows to {table_name}")
+def get_all_claims() -> list:
+    """Fetches all claims from The Graph."""
+    all_claims = []
+    offset = 0
+    while True:
+        claims = get_claims_with_offset(offset)
+        if not claims:
+            break
+        all_claims.extend(claims)
+        offset += 1000
+    print(f"The Graph shows a total of {len(all_claims)} hypercert claims.")
+    return all_claims
+
+
+def retrieve_ipfs_file(cid: str) -> dict:
+    """Fetches JSON data from IPFS using the given content identifier (CID)."""
+    url = f"https://cloudflare-ipfs.com/ipfs/{cid}"
+    print(f"Fetching: {url}")
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = json.loads(response.content)
+        return data
+    except requests.exceptions.HTTPError as e:
+        print(e)
+    return None
+
+
+def create_claim_record(claim, metadata):
+    """Creates a claim record from the given claim and metadata."""
+    if not metadata:
+        print(f"Skipping claim {claim['id']} due to missing metadata.")
+        return None
+
+    hypercert = metadata["hypercert"]
+    return {
+        "claimId": claim["id"],
+        "createdAt": int(claim["creation"]),
+        "title": metadata["name"],
+        "creatorAddress": claim["creator"],
+        "totalUnits": int(claim["totalUnits"]),
+        "date": timestamp_to_date_string(claim["creation"]),
+        "properties": metadata["properties"],
+        "hypercert": hypercert,
+    }
+
+
+def parse_claims(list_of_claims: list, existing_claims: list) -> list:
+    """Parses claims, filtering out existing ones, and creates claim records."""
+    print("Parsing new claims...")
+    claims = []
+    for claim in list_of_claims:
+        claim_id = claim['id']
+        if claim_id not in existing_claims:
+            cid = claim['uri'].replace("ipfs://", "")
+            metadata = retrieve_ipfs_file(cid)
+            claim_record = create_claim_record(claim, metadata)
+            if claim_record:
+                claims.append(claim_record)
+    print(f"Total of {len(claims)} new claims extracted.")
+    return claims
+
+
+def store_claims_in_supabase(claims: list):
+    """Stores the claim records in the Supabase table."""
+    for claim in claims:
+        supabase.table(TABLE_NAME).insert(claim).execute()
+
+
+def fetch_claim_ids_from_supabase() -> list:
+    """Fetches claim IDs from the Supabase table."""
+    data, _ = supabase.table(TABLE_NAME).select("claimId").execute()
+    claims = data[1]
+    claim_ids = [c["claimId"] for c in claims]
+    print(f"Supabase shows a total of {len(claim_ids)} hypercert claims.")
+    return claim_ids
+
+
+def append_to_csv_file(claims: list, csv_filepath: str = CSV_FILEPATH) -> None:
+    """Appends the claim records to the local CSV file."""
+    if not os.path.exists(csv_filepath):
+        df = pd.DataFrame(columns=claims[0].keys())
+        df.to_csv(csv_filepath, index_label="id")
+
+    df = pd.read_csv(csv_filepath, index_col="id")
+    for claim in claims:
+        df = df.append(claim, ignore_index=True)
+
+    df.to_csv(csv_filepath)
+
+
+def main():
+    claims_data = get_all_claims()
+    existing_claim_ids = fetch_claim_ids_from_supabase()
+    new_claims = parse_claims(claims_data, existing_claim_ids)
+
+    if new_claims:
+        store_claims_in_supabase(new_claims)
+        append_to_csv_file(new_claims)
+        fetch_claim_ids_from_supabase()
 
 
 if __name__ == "__main__":
-   #get_claims_data()
-   #make_initial_csv()
-   fetch_supabase_claims()
-   #update_supabase()
-
-
+    main()
