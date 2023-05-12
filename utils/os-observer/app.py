a869565480d7d16e69718c1e891072ad6f52b8cf
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2023-05-11 22:24:28 -0400

    feat: extract gitcoin projects from allo protocol (#639)
    * feat: extract gitcoin projects from allo protocol
    
    * fix: remove testing setup
    
    * fix: reduced token limits
    
    * chore: update gitignore
    
    * feat: add basic income indicator to dashboard

--- utils/os-observer/app.py
+++ utils/os-observer/app.py
@@ -11,14 +11,15 @@ from dash.exceptions import PreventUpdate
 from collections import Counter
 from datetime import date
 import pandas as pd
-from src.database import supabase_client
 import plotly.express as px
+
+from src.database import supabase_client
 
 #----------------------- GLOBALS ----------------------------------------------#
 
 supabase = supabase_client()
 
-DEFAULT_PROJECT_ID = 3
+DEFAULT_PROJECT_ID = 91
 DEFAULT_START_DATE = date(2022, 7, 1)
 DEFAULT_END_DATE   = date(2023, 5, 1)
 
@@ -27,7 +28,7 @@ DEFAULT_END_DATE   = date(2023, 5, 1)
 
 def get_project_mapping():
 
-    data, count = (supabase
+    response = (supabase
                     .table('projects')
                     .select('id, name, github_org, description')
                     .order('name')
@@ -39,42 +40,55 @@ def get_project_mapping():
             'github': project['github_org'],
             'description': project['description']
         }
-        for project in data[1]
+        for project in response.data
     }
 
     return mapping
 
 PROJECT_MAPPING = get_project_mapping()        
 
-def get_all_events_from_project(project_id, start_date, end_date):
 
-    data, count = (supabase
+def get_git_events_from_project(project_id, start_date, end_date):
+
+    response = (supabase
                    .table('events')
                    .select('*')
                    .eq('project_id', project_id)
+                   .eq('data_source', 'github')
                    .gte('timestamp', start_date)
                    .lte('timestamp', end_date)
                    .execute())
 
-    events_data = data[1]
+    events_data = response.data
 
     project = PROJECT_MAPPING.get(project_id)
     github_org = project.get('github')
 
-    events = []
-    for e in events_data:
-        if e['data_source'] != 'github':
-            continue
-        events.append({
+    return [
+        {
             'timestamp': e['timestamp'],
             'event_type': e['event_type'],
             'amount': e['amount'],
             'contributor': e['contributor'],
             'repo': e['details']['repository.name'],
             'github_org': github_org
-        })
+        }
+        for e in events_data
+    ]
 
-    return events
+
+def get_eth_events_from_project(project_id, start_date, end_date):
+
+    response = (supabase
+                   .table('events')
+                   .select('*')
+                   .eq('project_id', project_id)
+                   .eq('data_source', 'zerion')
+                   .gte('timestamp', start_date)
+                   .lte('timestamp', end_date)
+                   .execute())
+
+    return response.data
 
 
 def generate_treemap(events, groupby='repo'):
@@ -90,35 +104,46 @@ def generate_treemap(events, groupby='repo'):
         data_frame=df, 
         path=nodes, 
         values='amount', 
-        color='contributor'
+        color='contributor',
+        color_continuous_scale='RdBu'
     )
 
     fig.update_layout(
-        margin=dict(t=25, l=10, r=10, b=25)
+        margin=dict(t=20, l=10, r=10, b=10)
     )
     
     return fig
 
 
-def generate_kpis(project_id, events):
+def generate_kpis(project_id, git_events, eth_events):
 
     project = PROJECT_MAPPING.get(project_id)
     github_org = project.get('github')
     project_name = project.get('name')
 
-    contrib_counts = Counter([e['contributor'] for e in events])
-    event_counts = Counter(e['event_type'] for e in events)
-    repo_counts = Counter(e['repo'] for e in events)
+    contrib_counts = Counter([e['contributor'] for e in git_events])
+    event_counts = Counter(e['event_type'] for e in git_events)
+    repo_counts = Counter(e['repo'] for e in git_events)
+
+    income = 0
+    op_income = 0
+    for e in eth_events:
+        if e['event_type'] == 'income':
+            income += e['amount']
+            if e['details']['Buy Currency'] == 'OP':
+                op_income += e['amount']
 
     events_string = ", ".join([f"{k}s - {v}" for k,v in event_counts.items()])
 
     return [
-        html.H4(project_name),
-        html.P(
+        dcc.Markdown(
+            f"### {project_name} \n"\
             f"A total of {len(contrib_counts)} contributors made " \
-            f"{len(events)} contributions ({events_string}) in {len(repo_counts)} repos.",
+            f"{len(git_events)} contributions ({events_string}) in {len(repo_counts)} repos.\n\n" \
+            f"The project's primary wallet address also received ${income:,.0f} in income over this period.",
+            #f"including ${op_income:,.0f} worth of OP grants over this period.",
             style={'margin-bottom': "0px"}
-        )   
+        )
     ]
     
 
@@ -150,7 +175,7 @@ sidebar = html.Div(
                         {"label": pdata['name'], "value": pid}
                         for (pid, pdata) in PROJECT_MAPPING.items()
                     ],
-                    value=3,
+                    value=DEFAULT_PROJECT_ID,
                     style={"margin-left": "1px", "width": "14rem", "font-size": "12px"}
                 ),
                 html.Br(),
@@ -206,15 +231,15 @@ content = html.Div(
     children=[
         html.P(
             id='kpi-list',
-            style={'margin-left': "10px", 'margin-bottom': "0px"},
+            style={'height': '15vh', 'margin-left': "10px", 'margin-bottom': "0px"},
         ),
         dcc.Graph(
             id='treemap',
-            figure=generate_treemap(get_all_events_from_project(
+            figure=generate_treemap(get_git_events_from_project(
                     DEFAULT_PROJECT_ID, 
                     DEFAULT_START_DATE, 
                     DEFAULT_END_DATE)),
-            style={'height': '90vh', 'width': '100%'},
+            style={'height': '80vh', 'width': '100%'},
         )
     ],
     style=CONTENT_STYLE
@@ -248,17 +273,19 @@ def update_figure(project_id, groupby, start_date, end_date):
     if project_id is None:
         raise PreventUpdate
     
-    events = get_all_events_from_project(project_id, start_date, end_date)
-    if not len(events):
+    git_events = get_git_events_from_project(project_id, start_date, end_date)
+    if not len(git_events):
         raise PreventUpdate
 
-    fig = generate_treemap(events, groupby)
+    eth_events = get_eth_events_from_project(project_id, start_date, end_date)
+
+    fig = generate_treemap(git_events, groupby)
     descr = PROJECT_MAPPING[project_id]['description']
-    kpis = generate_kpis(project_id, events)
+    kpis = generate_kpis(project_id, git_events, eth_events)
 
     return [fig, descr, kpis]
 
 #----------------------- RUN APP ----------------------------------------------#
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
