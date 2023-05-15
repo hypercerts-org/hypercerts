c965ce334d96d2e1e3b97dcf9101abdcf9d6c342
ccerv1 <carl.cervone@gmail.com>
2023-05-14 22:00:26 -0400

    chore: add funding round schema

--- utils/os-observer/data/schema.sql
+++ utils/os-observer/data/schema.sql
@@ -35,3 +35,18 @@ create table
     constraint events_pkey1 primary key (id),
     constraint events_project_id_fkey foreign key (project_id) references projects (id)
   ) tablespace pg_default;
+
+
+  create table
+  public.funding (
+    id bigint not null,
+    ecosystem text null,
+    name text null,
+    address text null,
+    token text null,
+    amount bigint null,
+    start_date timestamp with time zone null,
+    end_date timestamp with time zone null,
+    details json null,
+    constraint funding_id_key unique (id)
+  ) tablespace pg_default;
