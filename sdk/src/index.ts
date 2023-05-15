758389b671672e83f9e01c3f26cef47d621b7e69
bitbeckers <code@bitbeckers.com>
2023-05-15 22:55:04 +0200

    feat(graph): updated autogen schema

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -17,8 +17,8 @@ import type { HypercertClaimdata } from "./types/claimdata.js";
 import type { HypercertMetadata } from "./types/metadata.js";
 import { formatHypercertData } from "./utils/formatter.js";
 import { validateClaimData, validateMetaData, validateAllowlist } from "./validator/index.js";
-import type { Allowlist, AllowlistEntry, ClaimProof } from "./types/hypercerts.js";
 import { TransferRestrictions } from "./types/hypercerts.js";
+import type { Allowlist, AllowlistEntry, ClaimProof } from "./types/hypercerts.js";
 
 export { validateMetaData, validateClaimData, validateAllowlist, formatHypercertData };
 
