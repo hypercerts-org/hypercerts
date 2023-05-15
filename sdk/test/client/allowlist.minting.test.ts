251767842f49b53fcb1b42cd96cf1aa83535c5aa
bitbeckers <code@bitbeckers.com>
2023-05-15 14:44:16 +0200

    fix(test): add mock instances to tests

--- sdk/test/client/allowlist.minting.test.ts
+++ sdk/test/client/allowlist.minting.test.ts
@@ -1,56 +1,65 @@
 import { jest } from "@jest/globals";
-import { MockProvider } from "ethereum-waffle";
+import { MockProvider, deployMockContract } from "ethereum-waffle";
 
 import HypercertClient from "../../src/client.js";
 import { getAllowlist } from "../helpers.js";
 
 import { MintingError } from "../../src/types/errors.js";
 import { ClaimProof } from "../../src/types/hypercerts.js";
-
-const provider = new MockProvider();
-const [wallet] = provider.getWallets();
-const signer = wallet.connect(provider);
+import { HypercertMinterABI } from "@hypercerts-org/hypercerts-protocol";
 
 describe("Claim from allowlist in HypercertClient", () => {
-  afterEach(() => {
+  const provider = new MockProvider();
+  const [wallet] = provider.getWallets();
+  const signer = wallet.connect(provider);
+
+  beforeEach(() => {
     provider.clearCallHistory();
     jest.clearAllMocks();
   });
 
   it("allows for minting with the correct proof", async () => {
-    const address = await wallet.getAddress();
+    const userAddress = await wallet.getAddress();
+    const mockMinter = await deployMockContract(wallet, HypercertMinterABI);
 
     const client = new HypercertClient({
-      config: { chainId: 5, provider, signer },
+      config: { chainId: 5, provider, signer, contractAddress: mockMinter.address },
     });
 
     expect(client.readonly).toBe(false);
 
-    const { merkleTree } = getAllowlist({ address });
+    const { merkleTree } = getAllowlist({ address: userAddress });
 
-    const spy = jest.spyOn(provider, "sendTransaction");
     const proof = merkleTree.getProof(0);
     const root = merkleTree.root;
 
+    await mockMinter.mock["mintClaimFromAllowlist(address,bytes32[],uint256,uint256)"]
+      .withArgs(userAddress, proof, 1, 100)
+      .returns();
+
     await client.mintClaimFractionFromAllowlist(1, 100, proof, root);
-    expect(spy).toBeCalledTimes(1);
+    //TODO determine underlying calls and mock those out. Some are provider simulation calls
+    expect(provider.callHistory.length).toBe(3);
   }, 30000);
 
   it("throws on minting with incorrect proof", async () => {
-    const address = await wallet.getAddress();
+    const userAddress = await wallet.getAddress();
+    const mockMinter = await deployMockContract(wallet, HypercertMinterABI);
 
     const client = new HypercertClient({
-      config: { chainId: 5, provider, signer },
+      config: { chainId: 5, provider, signer, contractAddress: mockMinter.address },
     });
 
     expect(client.readonly).toBe(false);
 
-    const { merkleTree } = getAllowlist({ address });
-
-    const spy = jest.spyOn(provider, "sendTransaction");
+    const { merkleTree } = getAllowlist({ address: userAddress });
 
     const proof = merkleTree.getProof(1);
     const root = merkleTree.root;
+
+    await mockMinter.mock["mintClaimFromAllowlist(address,bytes32[],uint256,uint256)"]
+      .withArgs(userAddress, proof, 1, 100)
+      .returns();
 
     try {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
@@ -59,26 +68,27 @@ describe("Claim from allowlist in HypercertClient", () => {
       expect(e).toBeInstanceOf(MintingError);
       expect((e as MintingError).message).toBe("Merkle proof verification failed");
     }
-    expect(spy).toBeCalledTimes(0);
+    //TODO determine underlying calls and mock those out. Some are provider simulation calls
+    expect(provider.callHistory.length).toBe(2);
     expect.assertions(4);
   }, 30000);
 
   it("allows for batch minting with the correct proofs", async () => {
-    const address = await wallet.getAddress();
+    const userAddress = await wallet.getAddress();
+    const mockMinter = await deployMockContract(wallet, HypercertMinterABI);
 
     const client = new HypercertClient({
-      config: { chainId: 5, provider, signer },
+      config: { chainId: 5, provider, signer, contractAddress: mockMinter.address },
     });
 
     expect(client.readonly).toBe(false);
 
-    const addressArray = new Array(3).fill(address);
+    const addressArray = new Array(3).fill(userAddress);
 
     const allowLists = addressArray.map((address) => {
       return getAllowlist({ address });
     });
 
-    const spy = jest.spyOn(provider, "sendTransaction");
     const claimProofs = allowLists.map((allowlist) => {
       const proof = allowlist.merkleTree.getProof(0);
       const root = allowlist.merkleTree.root;
@@ -91,8 +101,13 @@ describe("Claim from allowlist in HypercertClient", () => {
       } as ClaimProof;
     });
 
+    await mockMinter.mock["batchMintClaimsFromAllowlists(address,bytes32[][],uint256[],uint256[])"].returns();
+
     await client.batchMintClaimFractionFromAllowlist(claimProofs);
-    expect(spy).toBeCalledTimes(1);
+
+    expect(provider.callHistory.length).toBe(3);
+
+    expect.assertions(2);
   }, 30000);
 
   it("allows throws on batch minting with incorrect correct proofs", async () => {
