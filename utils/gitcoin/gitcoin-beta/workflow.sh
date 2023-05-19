c9d3c75e24c9025ba39d0dcb458e4f5c84834e75
ccerv1 <carl.cervone@gmail.com>
2023-05-18 23:49:27 -0400

    fix: chars being removed from cids

--- utils/gitcoin/gitcoin-beta/workflow.sh
+++ utils/gitcoin/gitcoin-beta/workflow.sh
@@ -7,17 +7,17 @@
 #python process_images.py
 
 # shorten project titles and descriptions
-python ai_description.py
+#python ai_helper.py
 
 # upload to web3.storage
 source .env
 path="../../../node_modules/@web3-storage/web3-storage-quickstart/put-files.js"
 
-r1=$(node $path --token="$WEB3_STORAGE_TOKEN" data/img/)
-img_cid=$(echo "$r1" | awk '{gsub(/\r/,""); $1=$1; print $NF}' | tr -d 'files\n')
+r=$(node $path --token="$WEB3_STORAGE_TOKEN" data/img/)
+img_cid=$(echo "$r" | awk -F'CID: ' '{print $2}' | tr -d '\n')
 
-r2=$(node $path --token="$WEB3_STORAGE_TOKEN" data/allowlists/)
-allowlist_cid=$(echo "$r2" | awk '{gsub(/\r/,""); $1=$1; print $NF}' | tr -d 'files\n')
+r=$(node $path --token="$WEB3_STORAGE_TOKEN" data/allowlists/)
+allowlist_cid=$(echo "$r" | awk -F'CID: ' '{print $2}' | tr -d '\n')
 
 echo "{\"img\": \"$img_cid\", \"allowlists\": \"$allowlist_cid\"}" > data/cids.json
 
