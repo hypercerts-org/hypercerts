35f8d9a9e98e2bc41650989a1f0988d5c6bf9c9a
Jason Moggridge <github_hou1@jmoggr.com>
2023-05-19 16:52:29 -0400

    Add prisma schema copying Carls sql schema

---
+++ utils/os-observer/prisma/migrations/20230519205134_copy_carls_schema/migration.sql
@@ -0,0 +1,57 @@
+-- CreateTable
+CREATE TABLE "projects" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "name" TEXT NOT NULL,
+    "github_org" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "projects_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "wallets" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "address" TEXT,
+    "project_id" UUID NOT NULL,
+    "type" TEXT,
+    "ens" TEXT,
+
+    CONSTRAINT "wallets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "events" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "project_id" UUID NOT NULL,
+    "event_time" TIMESTAMPTZ(3),
+    "event_type" TEXT,
+    "contributor" TEXT,
+    "amount" MONEY,
+    "details" JSONB,
+
+    CONSTRAINT "events_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "funding" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "ecosystem" TEXT,
+    "name" TEXT,
+    "address" TEXT,
+    "token" TEXT,
+    "amount" MONEY,
+    "start_date" TIMESTAMPTZ(3),
+    "end_date" TIMESTAMPTZ(3),
+    "details" JSONB,
+    "chain" TEXT,
+
+    CONSTRAINT "funding_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "wallets" ADD CONSTRAINT "wallets_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "projects"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "events" ADD CONSTRAINT "events_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "projects"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
