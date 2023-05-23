f93e1758fcea4868ec7217a3424b3b7cafb89cb5
Raymond Cheng <me@raymondcheng.net>
2023-05-22 21:19:13 -0700

    feat: zuzalu MVP (#705)
    * Adds fields for all 22 hypercerts
    * Adds conversion from percentage => USD => ETH => wei
    * Adds logic to log to supabase
    * Adds sendTransaction

--- frontend/components/zuzalu-purchase.tsx
+++ frontend/components/zuzalu-purchase.tsx
@@ -1,21 +1,59 @@
 import { DataProvider } from "@plasmicapp/loader-nextjs";
 import { Formik, FormikProps } from "formik";
+import _ from "lodash";
 import { useRouter } from "next/router";
 import React, { ReactNode } from "react";
 import { toast } from "react-toastify";
-import { useBalance, useNetwork } from "wagmi";
+import {
+  useBalance,
+  useNetwork,
+  usePrepareSendTransaction,
+  useSendTransaction,
+} from "wagmi";
+import { BigNumber, utils } from "ethers";
 import * as Yup from "yup";
 import { useAccountLowerCase } from "../hooks/account";
 import { useConfetti } from "./confetti";
 import { FormContext } from "./forms";
+import { supabase } from "../lib/supabase-client";
 
 /**
  * Constants
  */
 const FORM_SELECTOR = "currentForm";
-export const CHAIN_ID = 1;
+const DESTINATION_ADDRESS = "0x22E4b9b003Cc7B7149CF2135dfCe2BaddC7a534f";
+export const CHAIN_ID = 5;
+//const DESTINATION_ADDRESS = "zuzalu.eth";
+//export const CHAIN_ID = 1;
+const ETH_PRICE = 1847.79;
 export const MIN_PERCENTAGE = 0;
 export const MAX_PERCENTAGE = 100;
+
+// In USD
+const pricePerPercent: ZuzaluPurchaseFormData = {
+  percentBuyingZuzalu001: 200,
+  percentBuyingZuzalu002: 100,
+  percentBuyingZuzalu003: 400,
+  percentBuyingZuzalu004: 400,
+  percentBuyingZuzalu005: 300,
+  percentBuyingZuzalu006: 200,
+  percentBuyingZuzalu007: 100,
+  percentBuyingZuzalu008: 400,
+  percentBuyingZuzalu009: 50,
+  percentBuyingZuzalu010: 50,
+  percentBuyingZuzalu011: 150,
+  percentBuyingZuzalu012: 150,
+  percentBuyingZuzalu013: 150,
+  percentBuyingZuzalu014: 150,
+  percentBuyingZuzalu015: 150,
+  percentBuyingZuzalu016: 150,
+  percentBuyingZuzalu017: 150,
+  percentBuyingZuzalu018: 150,
+  percentBuyingZuzalu019: 150,
+  percentBuyingZuzalu020: 150,
+  percentBuyingZuzalu021: 150,
+  percentBuyingZuzalu022: 150,
+};
 
 const DEFAULT_FORM_DATA: ZuzaluPurchaseFormData = {
   percentBuyingZuzalu001: 0,
@@ -28,6 +66,18 @@ const DEFAULT_FORM_DATA: ZuzaluPurchaseFormData = {
   percentBuyingZuzalu008: 0,
   percentBuyingZuzalu009: 0,
   percentBuyingZuzalu010: 0,
+  percentBuyingZuzalu011: 0,
+  percentBuyingZuzalu012: 0,
+  percentBuyingZuzalu013: 0,
+  percentBuyingZuzalu014: 0,
+  percentBuyingZuzalu015: 0,
+  percentBuyingZuzalu016: 0,
+  percentBuyingZuzalu017: 0,
+  percentBuyingZuzalu018: 0,
+  percentBuyingZuzalu019: 0,
+  percentBuyingZuzalu020: 0,
+  percentBuyingZuzalu021: 0,
+  percentBuyingZuzalu022: 0,
 };
 
 interface ZuzaluPurchaseFormData {
@@ -41,6 +91,18 @@ interface ZuzaluPurchaseFormData {
   percentBuyingZuzalu008: number | string;
   percentBuyingZuzalu009: number | string;
   percentBuyingZuzalu010: number | string;
+  percentBuyingZuzalu011: number | string;
+  percentBuyingZuzalu012: number | string;
+  percentBuyingZuzalu013: number | string;
+  percentBuyingZuzalu014: number | string;
+  percentBuyingZuzalu015: number | string;
+  percentBuyingZuzalu016: number | string;
+  percentBuyingZuzalu017: number | string;
+  percentBuyingZuzalu018: number | string;
+  percentBuyingZuzalu019: number | string;
+  percentBuyingZuzalu020: number | string;
+  percentBuyingZuzalu021: number | string;
+  percentBuyingZuzalu022: number | string;
 }
 
 /**
@@ -80,6 +142,18 @@ const ValidationSchema = Yup.object().shape({
   percentBuyingZuzalu008: PercentageSchema,
   percentBuyingZuzalu009: PercentageSchema,
   percentBuyingZuzalu010: PercentageSchema,
+  percentBuyingZuzalu011: PercentageSchema,
+  percentBuyingZuzalu012: PercentageSchema,
+  percentBuyingZuzalu013: PercentageSchema,
+  percentBuyingZuzalu014: PercentageSchema,
+  percentBuyingZuzalu015: PercentageSchema,
+  percentBuyingZuzalu016: PercentageSchema,
+  percentBuyingZuzalu017: PercentageSchema,
+  percentBuyingZuzalu018: PercentageSchema,
+  percentBuyingZuzalu019: PercentageSchema,
+  percentBuyingZuzalu020: PercentageSchema,
+  percentBuyingZuzalu021: PercentageSchema,
+  percentBuyingZuzalu022: PercentageSchema,
 });
 
 export interface ZuzaluPurchaseFormProps {
@@ -96,21 +170,58 @@ export function ZuzaluPurchaseForm(props: ZuzaluPurchaseFormProps) {
   const { data: balance, isLoading: balanceLoading } = useBalance({
     address: address as `0x${string}`,
   });
+  const [value, setValue] = React.useState<BigNumber | undefined>();
+  const [wagmiErr, setWagmiErr] = React.useState<Error | undefined>();
+  const { config } = usePrepareSendTransaction({
+    request: {
+      to: DESTINATION_ADDRESS,
+      value,
+    },
+    onError(error) {
+      setWagmiErr(error);
+    },
+  });
+  const { sendTransaction } = useSendTransaction({
+    ...config,
+    onError(error) {
+      setWagmiErr(error);
+    },
+  });
 
   return (
     <div className={className}>
       <Formik
         validationSchema={ValidationSchema}
         validateOnMount={true}
+        validate={(values) => {
+          // console.log(values);
+          const parsedValues = _.mapValues(values, (v: string | number) =>
+            typeof v == "string" ? parseFloat(v) : v,
+          );
+          const cleanedValues = _.mapValues(parsedValues, (v: number) =>
+            isNaN(v) ? 0 : v,
+          );
+          const valuesInDollars = _.mergeWith(
+            cleanedValues,
+            pricePerPercent,
+            (percent, price) => percent * price,
+          );
+          const dollarArray = _.values(valuesInDollars);
+          const totalUSD = _.sum(dollarArray);
+          const totalETH = totalUSD / ETH_PRICE;
+          const totalWei = utils.parseEther(`${totalETH}`);
+          setValue(totalWei);
+        }}
         initialValues={{ ...DEFAULT_FORM_DATA }}
         enableReinitialize
         onSubmit={async (values, { setSubmitting }) => {
+          // Check for errors
           if (!address) {
-            console.log("User not connected");
+            console.warn("User not connected");
             toast("Please connect your wallet", { type: "error" });
             return;
           } else if (chain?.id !== CHAIN_ID) {
-            console.log(
+            console.warn(
               `On wrong network. Expect ${CHAIN_ID} Saw ${chain?.id}`,
             );
             toast("Please switch to the Ethereum network.", {
@@ -118,12 +229,32 @@ export function ZuzaluPurchaseForm(props: ZuzaluPurchaseFormProps) {
             });
             return;
           } else if (!balanceLoading && balance && balance.value.isZero()) {
-            console.log("No balance");
+            console.warn("No balance");
             toast(`No balance found for wallet ${address}`, { type: "error" });
             return;
           }
 
-          // TODO: transfer logic
+          // Write to supabase
+          const { error: supabaseError } = await supabase
+            .from("zuzalu-purchase")
+            .insert({
+              address,
+              values,
+            });
+          if (supabaseError) {
+            console.error("Supabase error", supabaseError);
+            toast(`Error writing to database`, { type: "error" });
+            return;
+          } else if (!sendTransaction) {
+            console.error("Unable to send transaction: ", wagmiErr?.message);
+            toast(wagmiErr?.message ?? "Unable to send transaction", {
+              type: "error",
+            });
+            return;
+          }
+
+          await sendTransaction();
+
           confetti &&
             (await confetti.addConfetti({
               emojis: ["🌈", "⚡️", "💥", "✨", "💫", "🌸"],
