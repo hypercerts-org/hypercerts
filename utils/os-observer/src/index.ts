bdf9c6b534db1ddd6bd25bfac50fc37040e007be
Jason Moggridge <github_hou1@jmoggr.com>
2023-05-25 13:50:05 -0400

    Add prettier

--- utils/os-observer/src/index.ts
+++ utils/os-observer/src/index.ts
@@ -1,28 +1,32 @@
-import { PrismaClient, EventType } from '@prisma/client'
+import { PrismaClient, EventType } from "@prisma/client";
 const prisma = new PrismaClient();
 
-async function getPointerForEventType<T extends EventType>(event_type: EventType): Promise<EventTypePointerLookup[T] | null> {
-    const pointer = await prisma.caching_pointer.findFirst({ where: { event_enum: { equals: event_type }}});
+async function getPointerForEventType<T extends EventType>(
+  event_type: EventType,
+): Promise<EventTypePointerLookup[T] | null> {
+  const pointer = await prisma.caching_pointer.findFirst({
+    where: { event_enum: { equals: event_type } },
+  });
 
-    if (!pointer) {
-        // TODO: error handling
-        return null;
-    }
+  if (!pointer) {
+    // TODO: error handling
+    return null;
+  }
 
-    return <any>pointer.pointer;
+  return <any>pointer.pointer;
 }
 
 type EventTypePointerLookup = {
-    [EventType.GITHUB_CREATED_PR]: GithubCreatedPrPointer
-}
+  [EventType.GITHUB_CREATED_PR]: GithubCreatedPrPointer;
+};
 
 type GithubCreatedPrPointer = {
-    lastFetch: string,
-}
+  lastFetch: string;
+};
 
 async function main() {
-    const pointer = await getPointerForEventType(EventType.GITHUB_CREATED_PR);
-    console.log(pointer);
+  const pointer = await getPointerForEventType(EventType.GITHUB_CREATED_PR);
+  console.log(pointer);
 }
 
 (() => main())();
