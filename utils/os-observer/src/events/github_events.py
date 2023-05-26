667bf34609c89157bdc1951b4961464a047f9560
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2023-05-26 09:42:40 -0400

    refactor: events database (#720)
    * refactor: scripts of populate the projects and events database
    
    * fix: update to latest schema
    
    * fix(scraper): better handling for chrome drivers
    
    * feat(db): handling for interruptions
    
    * fix: update scraper to new events schema
    
    * chore: prepare main function
    
    * fix(db): include zerion transactions as events

--- utils/os-observer/src/events/github_events.py
+++ utils/os-observer/src/events/github_events.py
@@ -1,13 +1,19 @@
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
-import json
+import logging
 import os
 import requests
 
-from .graphql_queries import QUERIES
+from events.graphql_queries import QUERIES
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='logging.log'
+)
 
 # -------------- HELPER FUNCTIONS -------------- #
-
 
 date_fmt = "%Y-%m-%dT%H:%M:%SZ"
 
@@ -16,20 +22,17 @@ def to_date(date):
 
 
 def find_dict_with_pageinfo(data):
-
     if isinstance(data, dict):
         for k, v in data.items():
             if v and 'pageInfo' in v:
                 return data[k]
-            else:
-                result = find_dict_with_pageinfo(v)
-                if result is not None:
-                    return result
+            result = find_dict_with_pageinfo(v)
+            if result is not None:
+                return result
     return None
 
 
 def flatten_dict(d):
-
     flattened_dict = {}
     for key, value in d.items():
         if isinstance(value, dict):
@@ -39,65 +42,66 @@ def flatten_dict(d):
             flattened_dict.update({f"{key}.{i}": item for i, item in enumerate(value)})
         else:
             flattened_dict[key] = value
-    return flattened_dict      
+    return flattened_dict
+
 
 # -------------- QUERY CONSTRUCTORS -------------- #
 
 def run_query_for_org(query_func, github_org, start_date, end_date):
-
-    load_dotenv()    
-    token = os.getenv('GITHUB_TOKEN')
+    load_dotenv()
+    github_token = os.getenv('GITHUB_TOKEN')
 
     end_cursor = "null"
     has_next_page = True
     rate_limit_hit = False
     events = []
+
     while has_next_page:
         query_string = query_func(
-            org=github_org, 
-            first=100, 
-            after=end_cursor, 
-            since=start_date, 
+            org=github_org,
+            first=100,
+            after=end_cursor,
+            since=start_date,
             until=end_date
         )
         response = requests.post(
-            'https://api.github.com/graphql', 
-            json={'query': query_string}, 
-            headers={'Authorization': f'token {token}'}
+            'https://api.github.com/graphql',
+            json={'query': query_string},
+            headers={'Authorization': f'token {github_token}'}
         )
-        response_json = response.json()
-        data = find_dict_with_pageinfo(response_json['data'])
+        response_json = response.json()        
+        data = find_dict_with_pageinfo(response_json.get('data'))
         if not data:
-            print(f"No items found for {github_org}.")
+            logging.info(f"No items found for {github_org}.")
             break
 
-        key = "edges" if "edges" in data.keys() else "nodes"
-        events.extend(data[key])
+        key = data.get('edges') and "edges" or "nodes"
+        events.extend(data.get(key, []))
 
         has_next_page = data['pageInfo']['hasNextPage']
         if has_next_page:
             end_cursor = data['pageInfo']['endCursor']
-            end_cursor = f'"{end_cursor}"'
+            # cursor must be passed in quotes
+            end_cursor = f'"{end_cursor}"' 
 
     if len(events) == 1000:
         rate_limit_hit = True
-        print("Hit max limit of 1000 results. Retrying")
-        return None, rate_limit_hit
+        logging.warning("Hit max limit of 1000 results. Retrying")
 
     return events, rate_limit_hit
-    
+
 
 def paginate_query_for_org(query_func, github_org, start_date, end_date):
-    
     start_dt = datetime.strptime(start_date, date_fmt)
-    max_dt = datetime.strptime(end_date, date_fmt) 
+    max_dt = datetime.strptime(end_date, date_fmt)
     delta_days = 180
     events = []
+
     while start_dt < max_dt:
         end_dt = start_dt + timedelta(days=delta_days)
         if end_dt > max_dt:
             end_dt = max_dt
-        print("Attempting:", to_date(start_dt), to_date(end_dt))
+        logging.info("Attempting: %s - %s", to_date(start_dt), to_date(end_dt))
         new_events, rate_limit_hit = run_query_for_org(query_func, github_org, to_date(start_dt), to_date(end_dt))
         if rate_limit_hit:
             delta_days //= 3
@@ -109,7 +113,6 @@ def paginate_query_for_org(query_func, github_org, start_date, end_date):
 
 
 def execute_org_query(query_idx, github_org, start_date, end_date):
-
     query = QUERIES[query_idx]
     query_func = query['func']
 
@@ -117,24 +120,26 @@ def execute_org_query(query_idx, github_org, start_date, end_date):
     if rate_limit_hit:
         events = paginate_query_for_org(query_func, github_org, start_date, end_date)
 
-    print(f"Query {query['name']}: {len(events)} events found for `{github_org}`")
+    logging.info("Query %s: %d events found for `%s`", query['name'], len(events), github_org)
 
     records = []
     for event in events:
-        details = flatten_dict(event)
-        records.append({
-            "timestamp": details[query['timestamp']],
-            "data_source": "github",
+        details = {
+            "source": "github",
+            "data": flatten_dict(event)
+        }
+        timestamp = details['data'].get(query.get('timestamp'))
+        contributor = details['data'].get(query.get('contributor'))
+        record = {
+            "event_time": timestamp or None,
             "event_type": query['name'],
-            "contributor": details.get(query['contributor']),
+            "contributor": contributor or None,
+            "amount": 1,
             "details": details
-        })
+        }
+        records.append(record)
     return records
 
 
 if __name__ == "__main__":
-
-    #START, END = '2022-01-01T00:00:00Z', '2023-04-22T00:00:00Z'
-    #test = execute_org_query(0, "NethermindEth", "2022-01-01T00:00:00Z", "2023-04-22T00:00:00Z")
-    test = execute_org_query(0, "truffle-box", "2022-01-01T00:00:00Z", "2023-04-22T00:00:00Z")  
-    
+    test = execute_org_query(0, "gitcoinco", "2022-01-01T00:00:00Z", "2023-04-22T00:00:00Z")
