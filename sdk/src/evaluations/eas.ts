fed518766fccf76e695c66f885dc29fcdc4e314f
bitbeckers <code@bitbeckers.com>
2023-05-27 14:07:03 +0200

    feat(eas): eas schemas as constants

--- sdk/src/evaluations/eas.ts
+++ sdk/src/evaluations/eas.ts
@@ -2,7 +2,7 @@ import { Offchain, SchemaEncoder } from "@ethereum-attestation-service/eas-sdk";
 import { TypedDataSigner } from "@ethersproject/abstract-signer";
 import { ethers } from "ethers";
 
-import { DEFAULT_CHAIN_ID } from "../constants.js";
+import { DEFAULT_CHAIN_ID, EAS_SCHEMAS } from "../constants.js";
 import { InvalidOrMissingError } from "../types/errors.js";
 import { DuplicateEvaluation, EvaluationData } from "../types/evaluation.js";
 
@@ -14,7 +14,6 @@ type EasEvaluatorConfig = {
 
 export default class EasEvaluator {
   offChain: Offchain;
-  schemaEncoder: SchemaEncoder;
   signer: TypedDataSigner;
 
   constructor({
@@ -23,7 +22,6 @@ export default class EasEvaluator {
     config?: EasEvaluatorConfig;
   }) {
     this.offChain = new Offchain({ address: config.address, chainId: config.chainId, version: "0.26" });
-    this.schemaEncoder = new SchemaEncoder("uint256 chainId, address contract, uint256 claimId, string uri");
     this.signer = config.signer;
   }
 
@@ -32,10 +30,11 @@ export default class EasEvaluator {
     if (evaluation.type === "duplicate") {
       const data = evaluation.evaluation as DuplicateEvaluation;
       const realHypercert = data.realHypercert;
+      const schemaEncoder = new SchemaEncoder(EAS_SCHEMAS["sepolia"].duplicate.schema);
 
       // Initialize SchemaEncoder with the schema string
       // TODO validate schema values
-      encodedData = this.schemaEncoder.encodeData([
+      encodedData = schemaEncoder.encodeData([
         { name: "chainId", value: realHypercert.chainId as string, type: "uint256" },
         { name: "contract", value: realHypercert.contract as string, type: "address" },
         { name: "claimId", value: realHypercert.claimId as string, type: "uint256" },
@@ -47,7 +46,7 @@ export default class EasEvaluator {
     }
 
     // Example schema on Sepolia
-    // https://sepolia.easscan.org/schema/view/0xbe6ab02c9907680b9b7d6eb8dac5b590eec64a30e863d6f2d1ce2d853990be27
+    // https://sepolia.easscan.org/schema/view/0xe542f797c9407ccb56e539d14c83718bf35c1d0f3c768bc2623aca56badfde51
     const offchainAttestation = await this.offChain.signOffchainAttestation(
       {
         // TODO who will be the recipient? The contract it points to? The creator?
