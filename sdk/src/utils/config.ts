4e6801de263b77c911a24e30050f4ff934f1953b
bitbeckers <code@bitbeckers.com>
2023-06-19 16:20:50 +0200

    chore(build): fresh build

--- sdk/src/utils/config.ts
+++ sdk/src/utils/config.ts
@@ -109,9 +109,18 @@ const getProvider = (overrides: Partial<HypercertClientConfig>) => {
   }
 
   const { rpcUrl } = getRpcUrl(overrides);
-  return rpcUrl
-    ? { provider: new ethers.providers.JsonRpcProvider(rpcUrl) }
-    : { provider: ethers.getDefaultProvider("goerli") };
+
+  const provider = rpcUrl ? new ethers.providers.JsonRpcProvider(rpcUrl) : ethers.getDefaultProvider("goerli");
+  provider.on("network", (newNetwork, oldNetwork) => {
+    // When a Provider makes its initial connection, it emits a "network"
+    // event with a null oldNetwork along with the newNetwork. So, if the
+    // oldNetwork exists, it represents a changing network
+    if (oldNetwork) {
+      window.location.reload();
+    }
+  });
+
+  return { provider };
 };
 
 const getSigner = (overrides: Partial<HypercertClientConfig>) => {
