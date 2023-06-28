2c7c40b11bd83372e6bec3f97fbf80099b1322e5
Reuven Gonzales <reuven.v.gonzales@gmail.com>
2023-06-28 00:34:09 -0700

    make a little tx client for hardhat
    This allows for us to have "network" traffic. A change we made that waits for N>1 confirmations caused issues that required this.

--- contracts/tasks/index.ts
+++ contracts/tasks/index.ts
@@ -4,6 +4,7 @@ export * from "./generate-address";
 export * from "./pause";
 export * from "./transfer-owner";
 export * from "./transfer-from-test-account";
+export * from "./test-tx-client";
 export * from "./upgrade";
 export * from "./unpause";
 export * from "./validate-upgrade";
