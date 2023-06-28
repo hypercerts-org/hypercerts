591c0cb13699fc50c0494791d509d48652e3f2f7
Reuven Gonzales <reuven.v.gonzales@gmail.com>
2023-06-28 12:17:34 -0700

    End-to-end test: Mint a hypercert (#878)
    * start test work
    
    * One working test. Second test fails
    
    * two token creation tests
    
    * make a little tx client for hardhat
    
    This allows for us to have "network" traffic. A change we made that waits for N>1 confirmations caused issues that required this.
    
    * docker: update to support tx-client
    
    * WIP
    
    * frontend: fix issue with useCheckWriteable
    
    Fixes usage in hypercerts minting only
    
    * fix all tests
    
    * Tests passing

--- docker/frontend.sh
+++ docker/frontend.sh
@@ -4,4 +4,11 @@ source /usr/src/app/node_modules/app.env.sh
 
 cd "${REPO_DIR}/frontend"
 
-yarn dev
+if [[ "$ENVIRONMENT" == "tests" ]]; then
+    echo "Building a production-like environment for testing"
+    yarn build
+    yarn start
+else
+    echo "Running the dev environment"
+    yarn dev
+fi
