5f5ed0299352ee1b081db65f2908e9eef8d640e7
Reuven Gonzales <reuven.v.gonzales@gmail.com>
2023-07-04 16:00:00 -0700

    Add missing env

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -5,20 +5,23 @@ source /usr/src/app/node_modules/app.env.sh
 
 cd "${REPO_DIR}"
 
-export DISPLAY=:99
+disp=:99
+screen=0
+export DISPLAY="${disp}.${screen}"
+
 apt-get install -y nginx 
 
 echo "starting nginx"
 nginx -c /etc/nginx/e2e_proxy.conf
 
 echo "starting xvfb"
-Xvfb "${DISPLAY}" -ac -listen tcp -screen 0 1200x800x24 &
+Xvfb "${disp}" -ac -listen tcp -screen "${screen}" 1200x800x24 &
 
 echo "starting fluxbox"
-fluxbox -display "${DISPLAY}" -screen 0 &
+fluxbox -display "${disp}" -screen "${screen}" &
 
 echo "starting vnc with password 'test'"
-x11vnc -display "${DISPLAY}.0" -forever -bg -passwd password 
+x11vnc -display "${DISPLAY}" -forever -bg -passwd password 
 
 yarn playwright install-deps
 yarn playwright install
