4022209592e2cc56451bc737440f3f74e45c60bb
Reuven Gonzales <reuven.v.gonzales@gmail.com>
2023-07-03 23:22:38 -0700

    remove temp code

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -5,13 +5,6 @@ source /usr/src/app/node_modules/app.env.sh
 
 cd "${REPO_DIR}"
 
-curl -s https://deb.nodesource.com/setup_18.x | bash
-apt-get install nodejs -y
-npm install -g yarn
-
-apt-get update
-apt-get install -y xvfb x11vnc fluxbox
-
 export DISPLAY=:1
 
 echo "starting xvfb"
