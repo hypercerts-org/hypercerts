2c5fcdc62c77b81414c4a7a106c6998c22191e86
Reuven Gonzales <reuven.v.gonzales@gmail.com>
2023-07-04 15:33:20 -0700

    clean up

--- docker/run_tests.sh
+++ docker/run_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euxo pipefail
+set -euo pipefail
 
 source /usr/src/app/node_modules/app.env.sh
 
@@ -23,6 +23,4 @@ x11vnc -display "${DISPLAY}.0" -forever -bg -passwd password
 yarn playwright install-deps
 yarn playwright install
 
-echo "chilling for now"
-sleep 10000
 yarn playwright test
