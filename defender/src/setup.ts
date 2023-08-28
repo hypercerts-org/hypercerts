91b736e005de8d719d0a083826b871c0353de456
bitbeckers <code@bitbeckers.com>
2023-08-28 22:36:09 +0200

    chore(lint): defender lint errors

--- defender/src/setup.ts
+++ defender/src/setup.ts
@@ -1,83 +1,42 @@
-import { createTask } from "./create-autotask.js";
-import { createSentinel } from "./create-sentinel.js";
-import { encodeName } from "./networks.js";
-import { ApiError } from "./errors.js";
 import config from "./config.js";
+import { ConfigError } from "./errors.js";
+import { reset } from "./reset.js";
+import { rollOut } from "./rollout.js";
+import { updateAutotask, updateSentinel } from "./update.js";
+import { AutotaskClient } from "defender-autotask-client";
+import { SentinelClient } from "defender-sentinel-client";
 
-export const rollOut = async () => {
-  return await Promise.all(
-    config.networks.map(async (network) => {
-      // On allowlist created
-      const autoTaskOnAllowlistCreated = await createTask(
-        encodeName(network, "on-allowlist-created"),
-        "on-allowlist-created",
-      );
-      if (!autoTaskOnAllowlistCreated) {
-        throw new ApiError(
-          encodeName(
-            network,
-            "Could not create autoTask for on-allowlist-created",
-          ),
-        );
-      }
-      await createSentinel({
-        name: encodeName(network, "AllowlistCreated"),
-        network: network,
-        eventConditions: [
-          { eventSignature: "AllowlistCreated(uint256,bytes32)" },
-        ],
-        autotaskID: autoTaskOnAllowlistCreated.autotaskId,
-      });
+const setup = async () => {
+  const autotaskClient = new AutotaskClient(config.credentials);
+  const sentinelClient = new SentinelClient(config.credentials);
 
-      // On batch minted
-      const autoTaskOnBatchMintClaimsFromAllowlists = await createTask(
-        encodeName(network, "batch-mint-claims-from-allowlists"),
-        "batch-mint-claims-from-allowlists",
-      );
-      if (!autoTaskOnBatchMintClaimsFromAllowlists) {
-        throw new ApiError(
-          encodeName(
-            network,
-            "Could not create autoTask for batch-mint-claims-from-allowlists",
-          ),
-        );
-      }
-      await createSentinel({
-        name: encodeName(network, "batchMintClaimsFromAllowlists"),
-        network: network,
-        autotaskID: autoTaskOnBatchMintClaimsFromAllowlists.autotaskId,
-        functionConditions: [
-          {
-            functionSignature:
-              "batchMintClaimsFromAllowlists(address,bytes32[][],uint256[],uint256[])",
-          },
-        ],
-      });
+  // Remove all old auto tasks and sentinels
+  const oldAutoTasks = await autotaskClient.list();
+  const oldSentinels = await sentinelClient.list();
 
-      // On single minted from allowlist
-      const autoTaskOnMintClaimFromAllowlist = await createTask(
-        encodeName(network, "mint-claim-from-allowlist"),
-        "mint-claim-from-allowlist",
-      );
-      if (!autoTaskOnMintClaimFromAllowlist) {
-        throw new ApiError(
-          encodeName(
-            network,
-            "Could not create autoTask for mint-claim-from-allowlist",
-          ),
-        );
-      }
-      await createSentinel({
-        name: encodeName(network, "mintClaimFromAllowlist"),
-        network: network,
-        autotaskID: autoTaskOnMintClaimFromAllowlist.autotaskId,
-        functionConditions: [
-          {
-            functionSignature:
-              "mintClaimFromAllowlist(address,bytes32[],uint256,uint256)",
-          },
-        ],
-      });
-    }),
-  );
+  let updates = false;
+
+  if (oldAutoTasks.items.length > 0) {
+    updates = true;
+    await updateAutotask();
+  }
+
+  if (oldSentinels.items.length > 0) {
+    updates = true;
+    await updateSentinel();
+  }
+
+  if (!updates) {
+    // Delete all sentinels and tasks first
+    await reset();
+
+    // Error out if no networks configured.
+    if (config.networks.length < 1) {
+      throw new ConfigError("No networks specified");
+    }
+
+    await rollOut();
+  }
 };
+
+void setup();
