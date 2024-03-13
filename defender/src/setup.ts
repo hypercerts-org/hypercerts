5bafc1451caacf8e977ef00f54038507375e24d3
bitbeckers <code@bitbeckers.com>
2024-03-13 20:10:01 +0100

    fix(defender): rollout on missing networks

--- defender/src/setup.ts
+++ defender/src/setup.ts
@@ -1,6 +1,6 @@
 import config from "./config";
 import { ApiError, ConfigError } from "./errors";
-import { NETWORKS } from "./networks";
+import { NETWORKS, getNetworkConfigFromName } from "./networks";
 import { reset } from "./reset";
 import { rollOut } from "./rollout";
 import { updateAutotask, updateSentinel } from "./update";
@@ -28,6 +28,18 @@ const setup = async () => {
   // Remove all old auto tasks and sentinels
   const oldAutoTasks = await autotaskClient.list();
   const oldSentinels = await sentinelClient.list();
+
+  const networksDeployed = oldAutoTasks.items
+    .map((task) => getNetworkConfigFromName(task.name).chainId)
+    .filter((value, index, array) => array.indexOf(value) === index);
+
+  const missingNetworks = networks.filter(
+    (network) => !networksDeployed.includes(network.chainId),
+  );
+
+  if (missingNetworks.length > 0) {
+    await rollOut(missingNetworks);
+  }
 
   let updates = false;
 
